50bcd464c1f85decbb655506cb80e7edb4572b83
Linda Ramos <ltramos7@gmail.com>
2021-03-18 16:27:50 -0500

    feat: added a post route

--- app/controllers/project_one_app.rb
+++ app/controllers/project_one_app.rb
@@ -3,4 +3,12 @@ class ProjectOneApp < Sinatra::Base
     erb :"blogs/new"
   end
 
+  post '/' do
+    blog = Blog.new(params["blog"])
+    if blog.save
+      redirect "/blogs/#{blog.id}"
+    else
+      erb :"blogs/new"
+    end
+  end
 end
