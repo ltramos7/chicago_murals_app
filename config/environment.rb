e9626b1e3d46b79100c7c14079fb7a19e801c13f
Linda Ramos <ltramos7@gmail.com>
2021-03-23 17:39:23 -0500

    bug: typo error

--- config/environment.rb
+++ config/environment.rb
@@ -18,5 +18,5 @@ class ProjectOneApp < Sinatra::Base
   set :method_override, true
   set :root, APP_ROOT
   set :views, File.join(APP_ROOT, "app", "views")
-  set :public_foler, File.join(APP_ROOT, "app", "public")
+  set :public_folder, File.join(APP_ROOT, "app", "public")
 end
