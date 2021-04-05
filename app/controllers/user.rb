8db5efd90605b577b9add3d373f551e7e138bfe8
Linda Ramos <ltramos7@gmail.com>
2021-04-05 17:54:07 -0500

    added routes for User class

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,26 @@
 class User < Sinatra::Base
-  get '/users' do 
+  get '/' do 
     erb :"users/new"
   end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :"user/show"
+  end
+
+  get '/users' do
+    @users = User.all
+    erb :users/show
+  end
+
+  post '/users' do
+    @user =  User.new(params["user"])
+    if @user.save
+      redirect "/users/#{@user.id}"
+    else
+      erb :"users/new"
+    end
+  end
+
 end
+
