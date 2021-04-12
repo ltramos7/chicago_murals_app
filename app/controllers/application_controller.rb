074c406c8c7a24a8422ff3966cd35120cd278ed3
Linda Ramos <ltramos7@gmail.com>
2021-04-12 16:13:03 -0500

    added a configuration to show all of my views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < Sinatra::Base
+  
   #Don't forget to update config.ru to see application_controller.rb
+
+  configure do
+    set :views, 'app/views'
+  end
+
   get '/' do
     "Welcome to Chicago Murals"
   end
