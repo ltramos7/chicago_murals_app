d9a4968559361447d8bda35b47ef44b7ef96a46d
Linda Ramos <ltramos7@gmail.com>
2021-04-16 18:19:40 -0500

    added patch route for artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,19 +8,26 @@ class ArtistsController < ApplicationController
     erb :"artists/new"
   end
 
-  post "/artists" do
-    @artist = Artist.create(params)
-    redirect "/artists/#{@artist.id}"
-  end
-
   get "/artists/:id" do
     @artist = Artist.find(params[:id])
     erb :"artists/show"
   end
 
-  patch "/artists/:id/edit" do
+  post "/artists" do
+    @artist = Artist.create(params)
+    redirect "/artists/#{@artist.id}"
+  end
+  
+  get "/artists/:id/edit" do
     @artist = Artist.find(params[:id])
     erb :"artists/edit"
+  end
+  
+  patch "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    @artist.update(first_name: params[:first_name], last_name: params[:last_name], alias: params[:alias])
+    redirect "/artists/#{@artist.id}"
+
   end
 
 end
