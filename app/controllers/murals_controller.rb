4c1e92a758378b908ef3a02413ceacd8a8fca94a
Linda Ramos <ltramos7@gmail.com>
2021-04-20 15:29:16 -0500

    added post mural route

--- app/controllers/murals_controller.rb
+++ app/controllers/murals_controller.rb
@@ -4,8 +4,17 @@ class MuralsController < ApplicationController
     erb :"murals/index"
   end
 
+  get "/murals/new" do
+    erb :"murals/new"
+  end
+
   get "/murals/:id" do
     @mural = Mural.find(params[:id])
     erb :"murals/show"
+  end
+
+  post "/murals" do
+    @mural = Mural.create(params)
+    redirect "/murals"
   end
 end
